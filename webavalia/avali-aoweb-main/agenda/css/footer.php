3e4e0e5c5caee3e16e08c66efe697cefe1d75559
Yasmin <64554444+YasminSilveira@users.noreply.github.com>
2021-06-30 17:09:05 -0300

    Add files via upload

---
+++ webavalia/avali-aoweb-main/agenda/css/footer.php
@@ -0,0 +1,197 @@
+ <link rel="stylesheet" href="css/estilo.css">
+
+       
+    <footer>
+       
+       <div class="container-footer-all">
+        
+            <div class="container-body">
+
+                <div class="colum1">
+                   
+
+                    <h1>Avaliação Final!!! </h1>
+                    
+                  
+
+                </div>
+
+               
+                        
+                        
+                    
+                    </div>
+                    <div class="row">
+                         
+                    <i  class = "fab fa-facebook" ></i>
+                        <label > <a href="https://www.facebook.com/yasmin.silveira.100">Facebook</a></label>
+                    </div>
+                    <div class="row">
+                       <i  class = "fab fa-github" ></i>
+                        <label><a href="https://github.com/YasminSilveira">Github</a></label>
+                    </div>
+                   
+                    <ul >
+       
+
+                </div>
+
+               
+
+            </div>
+        
+        </div>
+        
+        <div class="container-footer">
+               <div class="footer">
+                    <div class="copyright">
+                        © 2021 Todos os direitos reservados | <a href="">Magtimus</a>
+                    </div>
+
+                    <div class="information">
+                        <a href="">Informacion Compañia</a> | <a href="">Privacion y Politica</a> | <a href="">Terminos y Condiciones</a>
+                    </div>
+                </div>
+
+            </div>
+        
+    </footer>
+
+
+      <!--JavaScript at end of body for optimized loading-->
+      
+   <style>
+     
+
+
+footer{
+    width: 100%;
+    background:rgb(90, 10, 114);
+    color: white;   
+    
+}
+
+.container-footer-all{
+    width: 100%;
+    max-width: 1200px;
+    margin: auto;
+    padding: 40px;
+}
+
+.container-body{
+    display: flex;
+    justify-content: space-between;
+}
+
+.colum1{
+    max-width: 400px;
+}
+
+.colum1 h1{
+    font-size: 22px;
+}
+
+.colum1 p{
+    font-size: 14px;
+    color: #C7C7C7;
+    margin-top: 20px;
+}
+
+.colum2{
+    max-width: 400px;
+    
+}
+
+.colum2 h1{
+    font-size: 22px;
+}
+
+.row{
+    margin-top: 20px;
+    display: flex;
+}
+
+.row img{
+    width: 36px;
+    height: 36px;
+}
+
+.row label{
+    margin-top: 10px;
+    margin-left: 20px;
+    color: #C7C7C7;
+}
+
+.colum3{
+    max-width: 400px;
+}
+
+.colum3 h1{
+    font-size: 22px;
+}
+
+.row2{
+    margin-top: 20px;
+    display: flex;
+}
+
+.row2 img{
+    width: 36px;
+    height: 36px;
+}
+
+.row2 label{
+    margin-top: 10px;
+    margin-left: 20px;
+    max-width: 140px;
+}
+
+.container-footer{
+    width: 100%;  
+    background: #101010;
+}
+
+.footer{
+    max-width: 1200px;
+    margin: auto;
+    display: flex;
+    justify-content: space-between;  
+    padding: 20px;
+}
+
+.copyright{
+    color: #C7C7C7;
+}
+
+.copyright a{
+    text-decoration: none;
+    color: white;
+    font-weight: bold;
+}
+
+.information a{
+    text-decoration: none;
+    color: #C7C7C7;
+}
+
+
+@media screen and (max-width: 1100px){
+    
+    .container-body{
+        flex-wrap: wrap;
+    }
+    
+    .colum1{
+        max-width: 100%;
+    }
+    
+    .colum2,
+    .colum3{
+        margin-top: 40px;
+    }
+}
+   </style>
+
+      
+    </body>
+  </html>
---
+++ webavalia/avali-aoweb-main/agenda/index.php
@@ -0,0 +1,26 @@
+
+<?php
+    include_once 'css/header.php'; 
+?>
+
+<body background="img/galaxia.jpg">
+            <h1></h1>
+            <h1></h1>
+    <div class="box">
+          
+        <div class="itens">
+          
+            <img src="img/capainicial.jpg" alt="agenda">     
+        
+        </div>
+        
+        
+    </div>
+
+</body>
+<footer>
+<?php
+include_once 'css/footer.php'; 
+?>
+
+
